3deb3101d99384d3e30c32385d4c93cdfc0a8d2d
Othmane EL YAAKOUBI <48627755+othma125@users.noreply.github.com>
2023-11-30 15:25:56 +0100

    Update __init__.py

--- api/views/__init__.py
+++ api/views/__init__.py
@@ -7,8 +7,3 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api')
 from api.views.users import *
 from api.views.documents import *
 from api.views.text_blocks import *
-# from api.views.places import *
-# from api.views.places_reviews import *
-# from api.views.cities import *
-# from api.views.amenities import *
-# from api.views.places_amenities import *
