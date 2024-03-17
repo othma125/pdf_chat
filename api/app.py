baa308806ad9fd10ed65d32d2bf3b7c15ed2b0a3
Othmane EL YAAKOUBI <othma125@hotmail.com>
2024-03-17 00:26:48 +0000

    api/app.py

--- api/app.py
+++ api/app.py
@@ -49,4 +49,4 @@ def bad_request(error):
 
 if __name__ == "__main__":
     """ Main Function """
-    app.run(host='0.0.0.0', port=3000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
